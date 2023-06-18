87df5af2278937f0be1c39e21a9a739f319ae15b
kriloter <kriloter@pm.me>
2023-06-18 23:14:46 +0200

    Initial commit

---
+++ test/unit/test_app.py
@@ -0,0 +1,27 @@
+# test/unit/test_app.py
+
+import unittest.mock
+
+from redis import ConnectionError
+
+
+import pytest
+
+from page_tracker.app import app
+
+@pytest.fixture
+def http_client():
+    return app.test_client()
+
+@unittest.mock.patch("page_tracker.app.redis")
+def test_should_handle_redis_connection_error(mock_redis, http_client):
+    # Given
+    mock_redis.return_value.incr.side_effect = ConnectionError
+
+    # When
+    response = http_client.get("/")
+
+    # Then
+    assert response.status_code == 500
+    assert response.text == "Sorry, something went wrong \N{pensive face}"
+
